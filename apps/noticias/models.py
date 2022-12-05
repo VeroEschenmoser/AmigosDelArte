2f0a4b5c6b5152c7b26a6770dd9c04ec173e6122
VeroEschenmoser <eschenmoserv@gmail.com>
2022-12-05 13:00:15 -0300

    quitado ckeditor

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-from ckeditor.fields import RichTextField
+
 
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
-    contenido = RichTextField(blank=True, null=True)
+    contenido = models.TextField(blank=True, null=True)
     imagen = models.ImageField(null=True, blank=True, upload_to='static/img/')
     fecha = models.DateTimeField(auto_now_add=True)
     # autor = models.ForeignKey(User, on_delete=models.CASCADE)
